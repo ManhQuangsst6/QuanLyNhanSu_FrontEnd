5f3d254ddb551661ecc9496b9412891585e6b31c
Đào Mạnh Quang <92263633+ManhQuangsst6@users.noreply.github.com>
2023-11-01 01:08:36 +0000

    part 1

--- quanlynhansu/src/component/project/project-component.js
+++ quanlynhansu/src/component/project/project-component.js
@@ -2,60 +2,60 @@ import React, { useState, useEffect } from 'react';
 import { Space, Button, Table, Input, Form, Row, Col, } from 'antd';
 import { EditOutlined, DeleteOutlined, CaretRightOutlined, EyeOutlined, CheckOutlined } from '@ant-design/icons';
 import ProjectModelComponent from './project-modal';
+import {
+    GetProjects, DeleteProject, UpdateComplete
+} from "../../api/projectAPI";
+import ConvertDate from '../../common/ConvertDateShow';
 const { Column, ColumnGroup } = Table;
 const { Search } = Input;
-const columns = [
-    {
-        title: 'Name',
-        dataIndex: 'name',
-        width: '30%',
-    },
-    {
-        title: 'Age',
-        dataIndex: 'age',
-        width: '30%',
-    },
-    {
-        title: 'Address',
-        dataIndex: 'address',
-        width: '35%',
-    },
-    {
-        title: 'Tùy chọn',
-        dataIndex: 'action',
-        width: '5%',
-        render: (_, record) => (
-            <Space size="middle">
-                <a><EyeOutlined /></a>
-                <a><EditOutlined /></a>
-                <a><DeleteOutlined /></a>
-                <a><CaretRightOutlined /></a>
-                <a><CheckOutlined /></a>
-            </Space>
-        ),
-    },
 
-];
-const data = [];
-for (let i = 0; i < 46; i++) {
-    data.push({
-        key: i,
-        name: `Edward King ${i}`,
-        age: 32,
-        address: `London, Park Lane no. ${i}`,
-    });
-}
+
+
 const ProjectComponent = () => {
+    const columns = [
+        {
+            title: 'Name',
+            dataIndex: 'Name',
+            width: '20%',
+        },
+        {
+            title: 'DateStart',
+            dataIndex: 'DateStart',
+            width: '20%',
+            render: (item) => ConvertDate(item)
+        },
+        {
+            title: 'DateEnd',
+            dataIndex: 'DateEnd',
+            width: '20%',
+            render: (item) => {
+                return item ? ConvertDate(item) : ''
+            }
+        },
+        {
+            title: 'Description',
+            dataIndex: 'Description',
+            width: '35%',
+        },
+        {
+            title: 'Tùy chọn',
+            dataIndex: 'action',
+            width: '5%',
+            render: (_, record) => (
+                <Space size="middle">
+                    <a onClick={() => DeleteAProject(record.key)} ><EyeOutlined /></a>
+                    <a><EditOutlined /></a>
+                    <a><DeleteOutlined /></a>
+                    <a><CaretRightOutlined /></a>
+                    <a><CheckOutlined /></a>
+                </Space>
+            ),
+        },
+
+    ];
+    const [data, SetData] = useState([]);
+    const [isRender, SetIsRender] = useState(true);
     const [selectedRowKeys, setSelectedRowKeys] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const start = () => {
-        setLoading(true);
-        // ajax request after empty completing
-        setTimeout(() => {
-            setSelectedRowKeys([]);
-            setLoading(false);
-        }, 1000);
-    };
     const onSelectChange = (newSelectedRowKeys) => {
         console.log('selectedRowKeys changed: ', newSelectedRowKeys);
         setSelectedRowKeys(newSelectedRowKeys);
@@ -65,6 +65,31 @@ const ProjectComponent = () => {
         onChange: onSelectChange,
     };
     const hasSelected = selectedRowKeys.length > 0;
+    useEffect(() => {
+        if (isRender == true)
+            GetProjects('', '', '').then(res => {
+                let dataShow = res.data.map(item => {
+                    return {
+                        key: item.ID,
+                        Name: item.Name,
+                        DateStart: item.DateStart,
+                        DateEnd: item.DateEnd,
+                        Description: item.Description
+                    }
+                })
+                SetData(dataShow)
+            }).catch(e => {
+                console.log(e)
+            })
+        SetIsRender(false)
+    }, [isRender])
+    const DeleteAProject = (id) => {
+        DeleteProject(id).then(res => {
+            SetIsRender(true)
+        }).catch(e => {
+            console.log(e)
+        })
+    }
 
 
     return (
@@ -105,25 +130,23 @@ const ProjectComponent = () => {
                 <div style={{ display: 'flex', justifyContent: 'space-between' }}>
                     <h3>Danh sách dự án</h3>
                     <div style={{ display: 'flex', alignItems: 'center' }} >
-                        <ProjectModelComponent></ProjectModelComponent>
+                        <ProjectModelComponent ></ProjectModelComponent>
                         {/* <Button type="primary" ghost style={{ marginRight: 16 }}>Thêm </Button> */}
                         <Button danger>Xóa </Button>
                     </div>
                 </div>
 
                 <div
-                    style={{
 
-                    }}
                 >
 
-                    {/* <span
+                    <span
                         style={{
                             marginLeft: 8,
                         }}
                     >
                         {hasSelected ? `Selected ${selectedRowKeys.length} items` : ''}
-                    </span> */}
+                    </span>
                 </div>
                 <Table rowSelection={rowSelection} columns={columns} dataSource={data} >
 
